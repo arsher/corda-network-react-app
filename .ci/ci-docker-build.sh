11afbc5d943443ed2b85ac1f013a7682b6e0422d
jchrisjones <chris.jones@r3.com>
2020-01-10 15:28:37 +0000

    master: added docker commands

--- .ci/ci-docker-build.sh
+++ .ci/ci-docker-build.sh
@@ -15,6 +15,9 @@ then
   exit 1
 fi
 
+docker build -t $DOCKER_REGISTRY_TARGETTAG
+docker push $DOCKER_REGISTRY_TARGETTAG
+
 # Log out of Azure (our subscription only) as well as kubectl
 az logout --subscription $K8_DEPLOY_SUB
 
