b31f93c1064ba6229060c544a122fab266d2c076
jchrisjones <chris.jones@r3.com>
2020-01-10 15:47:17 +0000

    master: added path to dockerfile

--- .ci/ci-docker-build.sh
+++ .ci/ci-docker-build.sh
@@ -5,6 +5,13 @@ buildCounter=$1
 commitHash=$2
 triggeredBy=$3
 
+srcFolder='Site Only'
+
+LGREEN='\033[0;31m'
+NC='\033[0m' # No Color
+
+echo -e "${LGREEN}This is building from the $srcFolder source code\n${NC}"
+
 echo "Deploy of $buildCounter at revision $commitHash by $triggeredBy" at $(date)
 
 # Login to Azure for our subscription
@@ -15,7 +22,7 @@ then
   exit 1
 fi
 
-docker build -t $DOCKER_REGISTRY_TARGETTAG .
+docker build -t $DOCKER_REGISTRY_TARGETTAG "./$srcFolder/."
 docker push $DOCKER_REGISTRY_TARGETTAG
 
 # Log out of Azure (our subscription only) as well as kubectl
