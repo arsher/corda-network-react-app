6fde57d0d933ccc6dfc988c5f5dddc62148f9fe7
jchrisjones <chris.jones@r3.com>
2020-01-10 15:11:22 +0000

    ci-setup: testing azure login

--- .ci/ci-docker-build.sh
+++ .ci/ci-docker-build.sh
@@ -4,20 +4,18 @@ set -eu
 buildCounter=$1
 commitHash=$2
 triggeredBy=$3
-#K8_DEPLOY_USER=$4
-#K8_DEPLOY_PASSWORD=$5
 
-echo "TriggeredBy: $triggeredBy" 
+echo "Deploy of $buildCounter at revision $commitHash by $triggeredBy" at $(date)
 
 # Login to Azure for our subscription
-# az login --service-principal --username $K8_DEPLOY_USER --password $K8_DEPLOY_PASSWORD --tenant $K8_DEPLOY_TENANT
-# if [ $? -ne 0 ]
-# then
-#   echo "Failed to log in to Azure." >&2
-#   exit 1
-# fi
+az login --service-principal --username $K8_DEPLOY_USER --password $K8_DEPLOY_PASSWORD --tenant $K8_DEPLOY_TENANT
+if [ $? -ne 0 ]
+then
+  echo "Failed to log in to Azure." >&2
+  exit 1
+fi
 
 # Log out of Azure (our subscription only) as well as kubectl
-# az logout --subscription $K8_DEPLOY_SUB
+az logout --subscription $K8_DEPLOY_SUB
 
 exit 1
